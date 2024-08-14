a8a988164c852957cc4cbcd19b5c2cff066a0d6b
Xhanina <166377712+XhaninaIna@users.noreply.github.com>
2024-08-14 10:19:42 +0200

    create listing api route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
+import listingRouter from "./routes/listing.route.js";
 dotenv.config();
 
 //connected to database test
@@ -30,6 +31,7 @@ app.use("/api/user", userRouter);
 //create auth router
 app.use("/api/auth", authRouter);
 
+app.use("/api/listing", listingRouter);
 //create middleware
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
