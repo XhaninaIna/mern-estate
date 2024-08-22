08ffa06f3f848f1133303402bb25cf2d483e18e6
Xhanina <166377712+XhaninaIna@users.noreply.github.com>
2024-08-22 11:22:36 +0200

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,4 +1,28 @@
 import React from "react";
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="py-20 px-4 mx-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About CasaCharm Estate
+      </h1>
+      <p className="mb-4 text-slate-700">
+        Welcome to CasaCharm, where comfort meets elegance. Our estate is
+        designed to be a haven for those seeking a perfect blend of modern
+        luxury and timeless charm. Nestled in a serene location, CasaCharm
+        offers an exquisite living experience with meticulously crafted spaces,
+        lush surroundings, and a warm, inviting ambiance. Whether you're looking
+        for a tranquil retreat or a vibrant community, CasaCharm is the ideal
+        place to call home.
+      </p>
+      <p className="mb-4 text-slate-700">
+        At CasaCharm, we believe in creating more than just a place to live—we
+        create a lifestyle. Our estate is thoughtfully designed with premium
+        amenities, beautifully landscaped grounds, and a focus on
+        sustainability. We are committed to providing our residents with a
+        harmonious environment where every detail is taken care of. Discover the
+        difference at CasaCharm, where your comfort and satisfaction are our top
+        priorities.
+      </p>
+    </div>
+  );
 }
